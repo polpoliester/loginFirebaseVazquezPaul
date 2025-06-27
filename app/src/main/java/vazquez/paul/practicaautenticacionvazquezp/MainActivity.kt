1bbbd135a79bd5c2f225935c7d95c6d010171bdc
polpoliester <pol_poliester@hotmail.com>
2025-06-27 15:48:54 -0700

    Login

---
+++ app/src/main/java/vazquez/paul/practicaautenticacionvazquezp/MainActivity.kt
@@ -0,0 +1,20 @@
+package vazquez.paul.practicaautenticacionvazquezp
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        }
+    }
+}
